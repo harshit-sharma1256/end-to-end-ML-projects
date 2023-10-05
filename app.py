ed276c877141317be9a695d0ba81e730a481af72
Harshit Sharma <harshit2531937@gmail.com>
2023-10-05 17:40:14 +0530

    data ingestion commit

--- app.py
+++ app.py
@@ -1,4 +1,30 @@
 from src.E2E_Project.logger import logging
+from src.E2E_Project.exception import CustomException
+from src.E2E_Project.components.data_ingestion import DataIngestion
+from src.E2E_Project.components.data_ingestion import DataIngestionConfig
+# from src.E2E_Project.components.data_transformation import DataTransformationConfig,DataTransformation
+# from src.E2E_Project.components.model_tranier import ModelTrainerConfig,ModelTrainer
 
-if __name__ == "__main__":
-    logging.info("The execution has started.")
+import sys
+
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+
+    try:
+        #data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
+
+        #data_transformation_config=DataTransformationConfig()
+        # data_transformation=DataTransformation()
+        # train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+
+        ## Model Training
+
+        # model_trainer=ModelTrainer()
+        # print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
